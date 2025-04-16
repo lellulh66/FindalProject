fc40e4c2164853b01fd0fb5bc67e851b1abdedb1
lellulh66 <lianna.ellul.h66772@mcast.edu.mt>
2025-04-16 20:15:43 +0200

    Merged Score and Timer script
    Merged GameTimer & ScoreManager
    Fixed Lose Screen to actually fade in

--- Prototype/Assets/SCRIPTS/Rotator.cs
+++ Prototype/Assets/SCRIPTS/Rotator.cs
@@ -2,8 +2,10 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
+
 public class Rotator : MonoBehaviour
 {
+    /*use for overhead tokens when buttons are triggered*/
     public float speed = 45;
 
     // Update is called once per frame
--- Prototype/Assets/SCRIPTS/ScoreManager.cs
+++ Prototype/Assets/SCRIPTS/ScoreManager.cs
@@ -9,7 +9,27 @@ public class ScoreManager : MonoBehaviour
     public int targetScore = 1; // set the score needed to win
 
     public TMP_Text scoreText;
+    public float timeLimit = 60f; // Time in seconds
+    private float currentTime;
 
+    private bool gameOver = false;
+
+    void Start()
+    {
+        currentTime = timeLimit;
+    }
+    void Update()
+    {
+        if (gameOver) return;
+
+        currentTime -= Time.deltaTime;
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            TriggerGameOver();
+        }
+    }
     void UpdateScoreDisplay()
     {
         if (scoreText != null)
@@ -43,4 +63,27 @@ public class ScoreManager : MonoBehaviour
         winScreen.StartFade();
        
     }
+    public EndScreenAnim loseScreen;
+    void TriggerGameOver()
+    {
+        gameOver = true;
+        Debug.Log("Game Over! You Lose :(");
+        loseScreen.StartFade();
+    }
+    public float GetTimeRemaining()
+    {
+        return currentTime;
+    }
+
 }
+
+/*
+ 
+
+   
+
+   
+
+    
+   
+ */
